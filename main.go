27cf8043be192539434a79669b8a4c0903b06ab0
Nickhil Sethi <ns1635@nyu.edu>
2020-07-03 02:36:48 -0400

    simplify, using io.copy

--- main.go
+++ main.go
@@ -34,19 +34,8 @@ func writeForward(
 	incomingConnection net.Conn,
 	outgoingConnection net.Conn) {
 
-	defer outgoingConnection.Close()
-
 	for {
-		buffer := make([]byte, 5000)
-		dataSize, err := incomingConnection.Read(buffer)
-		if err != nil && err != io.EOF {
-			panic(err)
-		}
-		data := buffer[:dataSize]
-		_, err = outgoingConnection.Write(data)
-		if err != nil {
-			panic(err)
-		}
+		io.Copy(outgoingConnection, incomingConnection)
 	}
 }
 
@@ -54,19 +43,8 @@ func writeBackward(
 	outgoingConnection net.Conn,
 	incomingConnection net.Conn) {
 
-	defer incomingConnection.Close()
-
 	for {
-		buffer := make([]byte, 5000)
-		dataSize, err := outgoingConnection.Read(buffer)
-		if err != nil {
-			panic(err)
-		}
-		data := buffer[:dataSize]
-		_, err = incomingConnection.Write(data)
-		if err != nil {
-			panic(err)
-		}
+		io.Copy(incomingConnection, outgoingConnection)
 	}
 }
 
